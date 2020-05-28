f291dc5753a8db5496be3801ab56589dbcd8872d
ohaeori <0typhoon416@gmail.com>
2020-05-28 19:09:08 +0900

    create project

---
+++ gotMyFavorite/gotMyFavorite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gotMyFavorite
+//
+//  Created by ohaeori on 2020/05/28.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
