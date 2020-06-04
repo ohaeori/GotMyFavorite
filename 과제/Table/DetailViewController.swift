7bd95fe83822af2e74df6aaf2454d0d26439b674
donggyunan111 <58198219+donggyunan@users.noreply.github.com>
2020-06-04 23:08:00 +0900

    Add files via upload

---
+++ 과제/Table/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  Table
+//
+//  Created by andonggyun on 2020/06/04.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var receiveItem = ""
+
+    @IBOutlet var lblItem: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lblItem.text = receiveItem
+    }
+    
+
+    func reciveItem(_ item: String)
+    {
+        receiveItem = item
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
