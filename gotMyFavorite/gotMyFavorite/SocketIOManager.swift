2751e08499ec20bfc4dba355df070cb867704ee7
ohaeori <0typhoon416@gmail.com>
2020-06-12 21:20:52 +0900

    update Socket

--- gotMyFavorite/gotMyFavorite/SocketIOManager.swift
+++ gotMyFavorite/gotMyFavorite/SocketIOManager.swift
@@ -11,7 +11,7 @@ import SocketIO
 
 class SocketIOManager: NSObject {
     static let shared = SocketIOManager()
-     var manager = SocketManager(socketURL: URL(string: "http://localhost:9000")!, config: [.log(true), .compress])
+     var manager = SocketManager(socketURL: URL(string: "http://192.168.35.24:9000")!, config: [.log(true), .compress])
      var socket: SocketIOClient!
      override init() {
          super.init()
